b6c3543b79bb485781721991eda3fa6d9d7b476a
Jonkke <46763810+Jonkke@users.noreply.github.com>
2019-11-02 03:46:10 +0200

    Random change

--- src/test/java/ohtu/ohtuvarasto/VarastoTest.java
+++ src/test/java/ohtu/ohtuvarasto/VarastoTest.java
@@ -119,7 +119,7 @@ public class VarastoTest {
 
         varasto.otaVarastosta(2);
 
-        // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4
+        // varastossa pitäisi olla tilaa 10 - 8 + 2 eli 4 
         assertEquals(4, varasto.paljonkoMahtuu(), vertailuTarkkuus);
     }
 
